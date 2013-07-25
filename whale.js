3e159a223ba3100c1cb7b09b81567955ff0edde8
Chapman Shoop <chapman.shoop@gmail.com>
2013-07-25 13:04:33 -0700

    Remove js cruft from borrowed example

--- whale.js
+++ whale.js
@@ -66,104 +66,11 @@ function init() {
 	return circleShape
     }
 
-
-
-
-    // Triangle
-
-    var triangleShape = new THREE.Shape();
-    triangleShape.moveTo(  80, 20 );
-    triangleShape.lineTo(  40, 80 );
-    triangleShape.lineTo( 120, 80 );
-    triangleShape.lineTo(  80, 20 ); // close path
-
-
-
-
-    // Square
-
-    var sqLength = 80;
-
-    var squareShape = new THREE.Shape();
-    squareShape.moveTo( 0,0 );
-    squareShape.lineTo( 0, sqLength );
-    squareShape.lineTo( sqLength, sqLength );
-    squareShape.lineTo( sqLength, 0 );
-    squareShape.lineTo( 0, 0 );
-
-
-
-    // Arc circle
-
-    var arcShape = new THREE.Shape();
-    arcShape.moveTo( 50, 10 );
-    arcShape.absarc( 10, 10, 40, 0, Math.PI*2, false );
-
-    var holePath = new THREE.Path();
-    holePath.moveTo( 20, 10 );
-    holePath.absarc( 10, 10, 10, 0, Math.PI*2, true );
-    arcShape.holes.push( holePath );
-
-
-    // Smiley
-
-    var smileyShape = new THREE.Shape();
-    smileyShape.moveTo( 80, 40 );
-    smileyShape.absarc( 40, 40, 40, 0, Math.PI*2, false );
-
-    var smileyEye1Path = new THREE.Path();
-    smileyEye1Path.moveTo( 35, 20 );
-    // smileyEye1Path.absarc( 25, 20, 10, 0, Math.PI*2, true );
-    smileyEye1Path.absellipse( 25, 20, 10, 10, 0, Math.PI*2, true );
-
-    smileyShape.holes.push( smileyEye1Path );
-
-    var smileyEye2Path = new THREE.Path();
-    smileyEye2Path.moveTo( 65, 20 );
-    smileyEye2Path.absarc( 55, 20, 10, 0, Math.PI*2, true );
-    smileyShape.holes.push( smileyEye2Path );
-
-    var smileyMouthPath = new THREE.Path();
-    // ugly box mouth
-    // smileyMouthPath.moveTo( 20, 40 );
-    // smileyMouthPath.lineTo( 60, 40 );
-    // smileyMouthPath.lineTo( 60, 60 );
-    // smileyMouthPath.lineTo( 20, 60 );
-    // smileyMouthPath.lineTo( 20, 40 );
-
-    smileyMouthPath.moveTo( 20, 40 );
-    smileyMouthPath.quadraticCurveTo( 40, 60, 60, 40 );
-    smileyMouthPath.bezierCurveTo( 70, 45, 70, 50, 60, 60 );
-    smileyMouthPath.quadraticCurveTo( 40, 80, 20, 60 );
-    smileyMouthPath.quadraticCurveTo( 5, 50, 20, 40 );
-
-    smileyShape.holes.push( smileyMouthPath );
-
-
-    // Spline shape + path extrusion
-
-    var splinepts = [];
-    splinepts.push( new THREE.Vector2 ( 350, 100 ) );
-    splinepts.push( new THREE.Vector2 ( 400, 450 ) );
-    splinepts.push( new THREE.Vector2 ( -140, 350 ) );
-    splinepts.push( new THREE.Vector2 ( 0, 0 ) );
-
-    var splineShape = new THREE.Shape(  );
-    splineShape.moveTo( 0, 0 );
-    splineShape.splineThru( splinepts );
-
-
     // addShape( shape, color, x, y, z, rx, ry,rz, s );
     addShape( makeCircle(40), 0xff0011, 0, 125, -160, 0, 0, 0, 1 );
     addShape( makeCircle(60), 0x00ff11, 0, 125, -100, 0, 0, 0, 1 );
     addShape( makeCircle(80), 0x1100ff, 0, 125, -40, 0, 0, 0, 1 );
     addShape( makeCircle(60), 0x00ff11, 0, 125, 0, 0, 0, 0, 1 );
-    addShape( smileyShape,    0xff0011, -40, 85, 40, 0, 0, 0, 1 );
-    // addShape( makeCircle(40), 0xff0011, 0, 125, 40, 0, 0, 0, 1 );
-
-
-
-    //
 
     renderer = new THREE.CanvasRenderer( { antialias: true } );
     renderer.setSize( window.innerWidth, window.innerHeight );
@@ -171,16 +78,14 @@ function init() {
     renderer.sortElements = false;
     container.appendChild( renderer.domElement );
 
-
     document.addEventListener( 'mousedown', onDocumentMouseDown, false );
     document.addEventListener( 'touchstart', onDocumentTouchStart, false );
     document.addEventListener( 'touchmove', onDocumentTouchMove, false );
 
-    //
-
     window.addEventListener( 'resize', onWindowResize, false );
 
 }
+
 
 function onWindowResize() {
 
@@ -194,7 +99,6 @@ function onWindowResize() {
 
 }
 
-//
 
 function onDocumentMouseDown( event ) {
 
@@ -210,6 +114,7 @@ function onDocumentMouseDown( event ) {
 
 }
 
+
 function onDocumentMouseMove( event ) {
 
     mouseX = event.clientX - windowHalfX;
@@ -218,6 +123,7 @@ function onDocumentMouseMove( event ) {
     targetRotation = targetRotationOnMouseDown + ( mouseX - mouseXOnMouseDown ) * 0.02;
 
 }
+
 
 function animateThing( fromx, fromy, tox, toy, thingIndex, duration, easing, delay ) {
     setTimeout(function() {
@@ -233,8 +139,8 @@ function animateThing( fromx, fromy, tox, toy, thingIndex, duration, easing, del
 
 }
 
-function onDocumentMouseUp( event ) {
 
+function onDocumentMouseUp( event ) {
     document.removeEventListener( 'mousemove', onDocumentMouseMove, false );
     document.removeEventListener( 'mouseup', onDocumentMouseUp, false );
     document.removeEventListener( 'mouseout', onDocumentMouseOut, false );
@@ -254,9 +160,8 @@ function onDocumentMouseUp( event ) {
     animateThing(currentX, currentY, mouseX, mouseY, 7, 700, 'easeOutCubic', 100);
     animateThing(currentX, currentY - 40, mouseX - 40, mouseY - 40, 8, 700, 'easeOutCubic', 0);
     animateThing(currentX, currentY - 40, mouseX - 40, mouseY - 40, 9, 700, 'easeOutCubic', 0);
-
-
 }
+
 
 function onDocumentMouseOut( event ) {
 
@@ -265,6 +170,7 @@ function onDocumentMouseOut( event ) {
     document.removeEventListener( 'mouseout', onDocumentMouseOut, false );
 
 }
+
 
 function onDocumentTouchStart( event ) {
 
@@ -279,6 +185,7 @@ function onDocumentTouchStart( event ) {
 
 }
 
+
 function onDocumentTouchMove( event ) {
 
     if ( event.touches.length == 1 ) {
@@ -292,7 +199,6 @@ function onDocumentTouchMove( event ) {
 
 }
 
-//
 
 function animate() {
 
@@ -301,6 +207,7 @@ function animate() {
     render();
 
 }
+
 
 function render() {
 
